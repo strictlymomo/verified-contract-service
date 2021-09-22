a999e0310d17135b7de3a8a27ad0c1575348d679
strictlymomo <momo.araki@gmail.com>
2021-09-22 13:19:36 -0400

    Service

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ function buildURL(url) {
   return `https://cchain.explorer.avax.network/address/${url}/contracts`
 }
 
-
 async function handleRequest(request) {
   const { pathname } = new URL(request.url);
   
@@ -19,8 +18,21 @@ async function handleRequest(request) {
         
     const dataResponse = await fetch(buildURL(blahs[3]))
     const dataHTML = await dataResponse.text()
-    console.log(dataHTML)          
-  }
+    console.log(dataHTML)
 
-  return fetch("https://welcome.developers.workers.dev");
+    const data = {
+      hello: dataHTML
+    }
+  
+    const json = JSON.stringify(data, null, 2)
+  
+    return new Response(json, {
+        headers: {
+          "content-type": "application/json;charset=UTF-8"
+        }
+      })
+        
+  } else {
+    return fetch("https://welcome.developers.workers.dev");
+  }
 }
