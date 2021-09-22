0e5811803ab4805b7b05dd066ed97470ddf5777f
strictlymomo <momo.araki@gmail.com>
2021-09-22 12:57:09 -0400

    service

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
-addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
-})
-/**
- * Respond with hello worker text
- * @param {Request} request
- */
+addEventListener("fetch", (event) => {
+  event.respondWith(
+    handleRequest(event.request).catch(
+      (err) => new Response(err.stack, { status: 500 })
+    )
+  );
+});
+
+function buildURL(url) {
+  return `https://cchain.explorer.avax.network/address/${url}/contracts`
+}
+
+
 async function handleRequest(request) {
-  return new Response('Hello worker!', {
-    headers: { 'content-type': 'text/plain' },
-  })
+  const { pathname } = new URL(request.url);
+  
+  if (pathname.startsWith("/api/metadata")) {    
+    const blahs = pathname.split("/")    
+        
+    const dataResponse = await fetch(buildURL(blahs[3]))
+    const dataHTML = await dataResponse.text()
+    console.log(dataHTML)          
+  }
+
+  return fetch("https://welcome.developers.workers.dev");
 }
