cb64c42c5eedbcdeaac9983e1dbbf4b73ddc9057
strictlymomo <momo.araki@gmail.com>
2021-09-23 13:23:11 -0400

    webpack es6

--- index.js
+++ index.js
@@ -1,50 +1,46 @@
-addEventListener("fetch", (event) => {
+import { ElementHandler } from './utils/ElementHandler.js'
+
+console.log('ElementHandler', ElementHandler)
+
+addEventListener('fetch', event => {
   event.respondWith(
     handleRequest(event.request).catch(
-      (err) => new Response(err.stack, { status: 500 })
-    )
-  );
-});
+      err => new Response(err.stack, { status: 500 }),
+    ),
+  )
+})
 
-const hr = new HTMLRewriter()
-  .on("strong.mr-4.mb-2.text-dark", new ElementHandler().element())  
+const hr = new HTMLRewriter().on(
+  'strong.mr-4.mb-2.text-dark',
+  new ElementHandler(),
+)
 
-  // https://github.com/adamschwartz/web.scraper.workers.dev
-  class ElementHandler {
-    element(element) {
-      // An incoming element, such as `div`
-      console.log(`Incoming element: ${element.tagName}`)
-    }
-  
-    comments(comment) {
-      // An incoming comment
-    }
-  
-    text(text) {
-      // An incoming piece of text
-    }
-  } 
+// https://github.com/adamschwartz/web.scraper.workers.dev
 async function handleRequest(req) {
-  const { pathname } = new URL(req.url);
-  
-  if (pathname.startsWith("/api/metadata")) {
-    const res = await fetch(`https://cchain.explorer.avax.network/address/${(pathname.split("/"))[3]}/contracts`)
-    const dataHTML = await res.text()
-    
-    
-    
+  const { pathname } = new URL(req.url)
 
-    let json = JSON.stringify({
-      hello: dataHTML
-    }, null, 2)
-    
+  if (pathname.startsWith('/api/metadata')) {
+    const res = await fetch(
+      `https://cchain.explorer.avax.network/address/${
+        pathname.split('/')[3]
+      }/contracts`,
+    )
+    const dataHTML = await res.text()
+
+    let json = JSON.stringify(
+      {
+        hello: dataHTML,
+      },
+      null,
+      2,
+    )
+
     return new Response(json, {
-        headers: {
-          "content-type": "application/json;charset=UTF-8"
-        }
-      })
-        
+      headers: {
+        'content-type': 'application/json;charset=UTF-8',
+      },
+    })
   } else {
-    return fetch("https://welcome.developers.workers.dev");
+    return fetch('https://welcome.developers.workers.dev')
   }
 }
